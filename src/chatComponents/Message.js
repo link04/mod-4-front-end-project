8a7b7eb66c65e1421271906dde5e2b9357f7003a
MichaelRomero <liikemike@aim.com>
2019-04-17 11:02:41 -0400

    added style to messages and username

--- src/chatComponents/Message.js
+++ src/chatComponents/Message.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 const Message = (props) => {
   return (
-    <div className="room">
+    <div className="message-box">
       <div className="room-icon">
         <div className="icon"></div>
       </div>
-      <div className="room-content">
-        <p>{props.message.text}</p>
+      <div className="content-box">
+        <div className="content-info">
+          <p>Username</p>
+          <p>4:26PM</p>
+        </div>
+        <div className="content-message">
+          <p>{props.message.text}</p>
+        </div>
       </div>
     </div>
   )
