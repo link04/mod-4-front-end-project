c71914a49a3824b846007c4bcf917d07321a3c84
MichaelRomero <liikemike@aim.com>
2019-04-17 11:40:49 -0400

    styled users and room images, added padding for each message

--- src/chatComponents/Message.js
+++ src/chatComponents/Message.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Message = (props) => {
   return (
     <div className="message-box">
-      <div className="room-icon">
+      <div className="user-icon">
         <div className="icon"></div>
       </div>
       <div className="content-box">
         <div className="content-info">
-          <p>Username</p>
-          <p>4:26PM</p>
+          <p className="username">Username</p>
+          <p className="timestamp">4:26PM</p>
         </div>
         <div className="content-message">
           <p>{props.message.text}</p>
